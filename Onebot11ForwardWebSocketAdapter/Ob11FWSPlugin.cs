2c8de53b56f2193720b572b9049362a0daf5f0e9
Xeron <2398003522@qq.com>
2024-12-16 00:52:05 +0800

    [upd] Updated onebot 11 forward web socket plugin.

--- Onebot11ForwardWebSocketAdapter/Ob11FWSPlugin.cs
+++ Onebot11ForwardWebSocketAdapter/Ob11FWSPlugin.cs
@@ -1,42 +1,45 @@
 ﻿using AvaQQ.Core.Adapters;
+using AvaQQ.SDK;
+using AvaQQ.SDK.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System.Diagnostics;
 
 namespace Onebot11ForwardWebSocketAdapter;
 
-public class Plugin : AvaQQ.SDK.Plugin
+public class Ob11FWSPlugin : Plugin
 {
 	public override void OnPreLoad(IHostBuilder hostBuilder)
 	{
-		Debug.WriteLine(nameof(OnPreLoad));
-
 		hostBuilder.ConfigureServices(services =>
 		{
 			services.AddScoped<AdapterSelection>()
 				.AddScoped<AdapterSelectionView>();
 		});
+
+		FileLoggingExecutor.Information<Ob11FWSPlugin>($"{nameof(Ob11FWSPlugin)} preloaded.");
 	}
 
-	private ILogger<Plugin> _logger = null!;
+	private ILogger<Ob11FWSPlugin> _logger = null!;
 
 	public override void OnLoad(IServiceProvider services)
 	{
-		_logger = services.GetRequiredService<ILogger<Plugin>>();
+		_logger = services.GetRequiredService<ILogger<Ob11FWSPlugin>>();
 		_logger.LogInformation(nameof(OnLoad));
 
 		services.GetRequiredService<IAdapterSelectionProvider>()
 			.Register<AdapterSelection>();
+
+		_logger.LogInformation($"{nameof(Ob11FWSPlugin)} loaded.");
 	}
 
 	public override void OnPostLoad(IServiceProvider services)
 	{
-		_logger.LogInformation(nameof(OnPostLoad));
+		_logger.LogInformation($"{nameof(Ob11FWSPlugin)} post loaded.");
 	}
 
 	public override void OnUnload()
 	{
-		_logger.LogInformation(nameof(OnUnload));
+		_logger.LogInformation($"{nameof(Ob11FWSPlugin)} unloaded.");
 	}
 }
